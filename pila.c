fad3fab2403f5a5762048f0775af08ca8f039393
AxelGarciaTello <gata21022000@hotmail.com>
2019-02-09 15:47:18 -0600

    Se agrego la función copiarPila y n-ésimo elemento

--- pila.c
+++ pila.c
@@ -33,8 +33,8 @@ int pilaVacia(PILA p){
 //Función para insertar un dato en la pila
 void insertarPila(PILA *p, TipoDato x){
      if(pilaLlena(*p)){
-                       printf("Error. Pila llena.\n");
-                       exit(-1);
+		printf("Error. Pila llena.\n");
+        exit(-1);
      }
      p->Cima=p->Cima+1;
      p->ListaPila[p->Cima]=x;
@@ -42,24 +42,53 @@ void insertarPila(PILA *p, TipoDato x){
 
 //Función para quitar un dato en la pila
 TipoDato quitarPila(PILA *p){
-         if(pilaVacia(*p)){
-                           printf("Error. Pila vacia.\n");
-                           exit(-1);
-         }
-         TipoDato aux=p->ListaPila[p->Cima];
-         p->Cima=p->Cima-1;
+	if(pilaVacia(*p)){
+		printf("Error. Pila vacia.\n");
+        exit(-1);
+    }
+    TipoDato aux=p->ListaPila[p->Cima];
+    p->Cima=p->Cima-1;
 }
 
 //Función para ver el dato en la pila
 TipoDato cimaPila(PILA p){
-         if(pilaVacia(p)){
-                          printf("Error. Pila vacia.\n");
-                          exit(-1);
-         }
-         return p.ListaPila[p.Cima];
+    if(pilaVacia(p)){
+        printf("Error. Pila vacia.\n");
+        exit(-1);
+    }
+    return p.ListaPila[p.Cima];
 }
 
 //Función para eliminar la pila
 void eliminarPila(PILA *p){
      p->Cima=-1;
+}
+
+//Función para copiar el contenido de una pila
+void copiarPila(PILA A, PILA *B){
+	PILA Aux;
+	crearPila(&Aux);
+	while(!pilaVacia(A)){
+		insertarPila(&Aux, quitarPila(&A));
+	}
+	while(!pilaVacia(Aux)){
+		insertarPila(B, quitarPila(&Aux));
+	}
+}
+
+//Función para mostrar el n-ésimo elemento
+TipoDato elemento(PILA A, int n){
+	TipoDato aux;
+	int count=0;
+	while(!pilaVacia(A)&&count<n){
+		aux=quitarPila(&A);
+		count++;
+	}
+	if(count==n){
+		return aux;
+	}
+	else{
+		printf("Error\n");
+		exit(-1);
+	}
 }
